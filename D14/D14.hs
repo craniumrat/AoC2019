7cbb36044b5acd6a08f434ec2f4032d212ea7f51
Mridul Pentapalli <mridulp@gmail.com>
2020-06-12 11:28:09 +0530

    Finally working...

--- D14/D14.hs
+++ D14/D14.hs
@@ -14,10 +14,13 @@ import qualified Text.Megaparsec.Char.Lexer as L
 import Data.Void
 import Control.Monad (void)
 import Control.Applicative as CA
-import qualified Data.Set as S
 import qualified Data.Map.Strict as M
+import Data.List (sortBy, groupBy)
+import Data.Function (on)
 
 type Parser = Parsec Void String
+
+traceShow' a = trace (show a) a
 
 sc :: Parser ()
 sc = L.space (skipSome spaceChar) CA.empty CA.empty
@@ -26,34 +29,74 @@ sepP = L.symbol sc "=>"
 commaP = L.symbol sc ","
 
 data Chemical = Chemical { _name :: String, _quantity :: Integer } deriving (Show, Ord, Eq)
-data Reaction = Reaction { _chemical :: Chemical, _ingredients :: S.Set Chemical } deriving (Show, Ord, Eq)
+data Reaction = Reaction { _chemical :: Chemical, _ingredients :: [Chemical] } deriving (Show, Ord, Eq)
 
 toChemical :: Parser Chemical
 toChemical = do
   q <- L.lexeme sc L.decimal
   n <- L.lexeme sc $ CA.some alphaNumChar
   pure $ Chemical n q
-  
+
 toChemicals :: Parser [Chemical]
 toChemicals = toChemical `sepBy` commaP
+
+chemicalToPair :: Chemical -> (String, Integer)
+chemicalToPair c = (_name c, _quantity c)
 
 toReaction :: Parser Reaction
 toReaction = do
   cs <- toChemicals
   void $ sepP
   c <- toChemical
-  pure $ Reaction c (S.fromList cs)
+  pure $ Reaction c cs
 
 parseReaction :: String -> Reaction
 parseReaction s = case parse toReaction "" s of
   Left _error -> error $ "Failed to parse"
   Right reaction -> reaction
 
-answer1 :: String -> String
-answer1 contents = show reactions
+type Ingredients = M.Map String (Integer, [(String, Integer)])
+
+reactionsToIngredients :: [Reaction] -> Ingredients 
+reactionsToIngredients reactions = foldl convert M.empty reactions
   where
-    reactions = S.fromList $ parseReaction <$> lines contents
-  
+    convert m reaction = M.insert key value m
+      where
+        key = _name._chemical $ reaction
+        value  = (_quantity._chemical $ reaction, chemicalToPair <$> _ingredients reaction)
+    
+answer1 :: String -> String
+answer1 contents = show output
+  where
+    reactions = reactionsToIngredients $ parseReaction <$> lines contents
+    output = quantity [("FUEL", 1)] reactions
+
+{- Given a [(Integer, String)] pair, grab the first one.
+   In the map, find the ingredients list and multiply the quantity with the fst of the value. Once we do that, group by name, add the values and repeat. The only key with no value in map will be "ORE". If we see an "ORE", leave it and go to the next one
+-}
+
+quantity :: [(String, Integer)] -> Ingredients -> [(String, Integer)]
+quantity (c:cs) reactions =
+  if (snd c <= 0) then quantity (traceShow' $ (cs ++ [c])) reactions
+  else
+    case M.lookup (fst c) reactions of
+      Nothing -> if end (c:cs) then (c:cs)  else quantity (cs ++ [c]) reactions
+      Just (weight, ingredients) ->
+        let
+          multiplier = traceShow' $ (ceiling $ fromIntegral (snd c) / fromIntegral weight)
+          negative = (traceShow' (snd c)) - (traceShow' $ (multiplier * weight))
+          neg = if (negative /= 0) then (traceShow' [(fst c, negative)]) else (traceShow' [])
+          weighted = (map (\(k, v) -> (k, multiplier * v)) ingredients) ++ neg
+          sorted = sortBy (compare `on` fst) $ cs ++ weighted
+          grouped = groupBy ((==) `on` fst) sorted
+          mapped = map (\chemicals -> foldl1 (\(k, v1) (_, v2) -> (k, v1 + v2)) chemicals)  grouped
+        in
+          quantity (traceShow' mapped) reactions
+      
+
+end :: [(String, Integer)] -> Bool
+end [] = True
+end (x@(chem, q):xs) = if (chem /= "ORE" && q > 0) then False else end xs
 
 main :: IO()
 main = do
